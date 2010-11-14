7a437053fd8f98838a6b41f2f57775297fa90946
Tomas Celizna <tomas.celizna@gmail.com>
2010-11-14 20:33:52 +0100

    readme

--- lib/mongo_mapper/plugins/version.rb
+++ lib/mongo_mapper/plugins/version.rb
@@ -2,7 +2,7 @@ class MongoMapper::Plugins::Versioned::Version
 
   include MongoMapper::Document
   
-  # returns all versions of document specified by versioned_id (or self)
+  # returns all versions of document specified by versioned_id or self
   def self.versions_of(value)
     value.respond_to?(:versioned_id) ? versioned_id = value.versioned_id : versioned_id = value
     where(:versioned_id => versioned_id).order(:updated_at.asc)
